ed039affbb03ba530b9aa89298c36200167593cc
Herick <herickguillen@gmail.com>
2024-02-09 18:25:28 -0500

    swagger
    Swagger documented

---
+++ controllers/theaters.js
@@ -0,0 +1,19 @@
+const mongodb = require("../data/data");
+const Object = require("mongodb").ObjectId;
+
+// This is the idea of how it should look the creation and
+// the information that we will handle.
+
+// "id": numeric
+// "name": string
+// "location": string
+  
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db().collection("theaters").find();
+    result.toArray().then((theaters) => {
+        res.setHeader("Content-type", "application/json");
+        res.status(200).json(theaters);
+    });
+};
+
+module.exports = { getAll };
