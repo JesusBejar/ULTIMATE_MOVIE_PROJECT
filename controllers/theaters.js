d32de08aec123e9dd58f45cd6b3588c3583111dd
j-adrian-lopez <adrj156@byui.edu>
2024-02-10 12:13:10 -0300

    Change comments filename. Added get functionality

--- controllers/theaters.js
+++ controllers/theaters.js
@@ -1,5 +1,5 @@
-const mongodb = require("../data/data");
-const Object = require("mongodb").ObjectId;
+const mongodb = require('../data/data');
+const Object = require('mongodb').ObjectId;
 
 // This is the idea of how it should look the creation and
 // the information that we will handle.
@@ -7,13 +7,17 @@ const Object = require("mongodb").ObjectId;
 // "id": numeric
 // "name": string
 // "location": string
-  
+
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection("theaters").find();
-    result.toArray().then((theaters) => {
-        res.setHeader("Content-type", "application/json");
-        res.status(200).json(theaters);
-    });
+  const result = await mongodb
+    .getDatabase()
+    .db('sample_mflix')
+    .collection('theaters')
+    .find();
+  result.toArray().then((theaters) => {
+    res.setHeader('Content-type', 'application/json');
+    res.status(200).json(theaters);
+  });
 };
 
 module.exports = { getAll };
