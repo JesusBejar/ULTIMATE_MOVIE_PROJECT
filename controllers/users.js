949844cd79acc63e752a70e2675f220d1c3de1a0
Yoli-Buendia <bue17003@byui.edu>
2024-02-15 20:41:04 -0600

    added getById to the users

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const mongodb = require('../data/data');
+  const mongodb = require('../data/data');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
@@ -16,4 +16,31 @@ const getAll = async (req, res) => {
   });
 };
 
-module.exports = { getAll };
+const getById = async (req, res, next) => {
+
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid id to get theater information.');
+      return;
+    }
+
+    const departmentID = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDatabase()
+      .db('sample_mflix')
+      .collection('users')
+      .find({ _id: departmentID })
+      .toArray();
+
+    if (!result || result.length === 0) {
+      throw notFoundError();
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result[0]);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAll, getById };
