973033ef56b46908b4e5574a9f3bef5d1fb14a80
j-adrian-lopez <adrj156@byui.edu>
2024-02-20 23:37:15 -0300

    update index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const passport = require("passport");
+const passport = require('passport');
 
 router.use('/', require('./swagger.js'));
 
@@ -9,13 +9,18 @@ router.use('/comments', require('./comments'));
 router.use('/movies', require('./movies'));
 
 // login
-router.get("/login", passport.authenticate("github", (req, res) => {}));
+router.use(
+  '/login',
+  passport.authenticate('github', (req, res) => {})
+);
 // logout
-router.get('/logout', function(req, res, next) {
-    req.logout(function(err){
-        if (err) {return next (err);}
-        res.redirect('/');
-    })
-})
+router.use('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
