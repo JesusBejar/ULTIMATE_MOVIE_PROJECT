8331236f08640d0408645824548ccf7988e53260
j-adrian-lopez <adrj156@byui.edu>
2024-02-20 22:55:49 -0300

    Add getAll to movies routes

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const moviesController = require('../controllers/movies');
 const { isAuthenticated } = require('../middleware/authenticate');
 
 // Routes for each request (CRUD)
-// get all
+router.get('/', moviesController.getAll);
 router.get('/:id', moviesController.getSingleMovie);
 router.post('/', isAuthenticated, validator, moviesController.createMovie);
 router.put('/:id', isAuthenticated, validator, moviesController.updateMovie);
